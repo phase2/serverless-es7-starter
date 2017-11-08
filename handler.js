d8ded64ef54ec49d8c07ded1a54383641697cbaa
Frank Febbraro <frank@phase2technology.com>
2017-11-08 13:22:44 -0800

    updated based on more research

--- handler.js
+++ handler.js
@@ -1,16 +1,22 @@
 
-if (!global._babelPolyfill) {
-  require('babel-polyfill');
-}
 
-export const hello = (event, context, callback) => {
+export const hello = async (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Hello Whirled!'}))}`,
+      input: event,
+    }),
+  };
 
-  const greeting = 'Hello Whirled';
-  const message = `Go Serverless Webpack: ${greeting}`;
-  console.log(greeting);
-  console.log(message);
-  console.log(event);
-  console.log(context);
+  callback(null, response);
 
-  callback(null, message);
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
+  setTimeout(() => {
+    resolve(`${rest.copy} (with a delay)`);
+  }, time * 1000)
+);
--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,22 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
+  // Generate sourcemaps for better error messages
+  devtool: 'source-map',
+  // Ignore everything in the node_modules folder
   externals: [nodeExternals()],
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
     filename: 'handler.js', // this should match the first part of function handler in serverless.yml
   },
+  // Run babel on all .js files and skip those in node_modules
   module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loaders: ["babel-loader"]
-      }
-    ]
+    rules: [{
+      test: /\.js?$/,
+      loaders: ["babel-loader"],
+      include: __dirname,
+      exclude: /node_modules/,
+    }]
   }
 };
